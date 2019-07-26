efc198f7e472982630fe91900d7b772536963770
MarcoSants <marcochel@gmail.com>
2019-07-25 21:35:02 -0300

    Lessons x -> 45

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,1 +1,13 @@
-const person = () => {};
+import React from "react";
+
+const Person = props => {
+  return (
+    <div>
+      <p>Hi {props.name}</p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.change} />
+    </div>
+  );
+};
+
+export default Person;
